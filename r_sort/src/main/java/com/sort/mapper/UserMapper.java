62bc82c229d2c579d70d0ed63c3838e71dcaf8fc
zjm <zhangjiaming@insta360.com>
2024-03-06 22:55:17 +0800

    添加usermapper

---
+++ r_sort/src/main/java/com/sort/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.sort.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.sort.entity.Admin;
+import com.sort.entity.User;
+
+/**
+ * @Author: zhazhaming
+ * @Date: 2024/01/07/11:35
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
